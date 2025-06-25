16de3821282ae17569dd948acc761b93f888cc04
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2025-06-25 15:49:50 +0200

    feat: returns the reviews attached to the services

--- src/lib/services-queries.ts
+++ src/lib/services-queries.ts
@@ -1,6 +1,6 @@
 import { eq } from "drizzle-orm";
 
-import { db, developers, services, users } from "@/lib/schema";
+import { db, developers, reviews, services, users } from "@/lib/schema";
 
 export async function getServicesForUser(userId: string) {
   // As client
@@ -15,7 +15,20 @@ export async function getServicesForUser(userId: string) {
     .innerJoin(developers, eq(services.developerId, developers.id))
     .where(eq(services.clientId, userId));
 
-  // As developer
+  const allReviews = await db
+    .select()
+    .from(reviews)
+    .innerJoin(services, eq(reviews.serviceId, services.id));
+
+  asClient.forEach((service) => {
+    // We're filtering allReviews where the nested review object's serviceId matches the current service's ID.
+    // We then map to get just the review object itself, not the { review: ..., service: ... } wrapper.
+    /* @ts-expect-error new property*/
+    service.reviews = allReviews
+      .filter((reviewItem) => reviewItem.review.serviceId === service.id)
+      .map((reviewItem) => reviewItem.review);
+  });
+
   const asDeveloper = await db
     .select({
       id: services.id,
@@ -26,6 +39,13 @@ export async function getServicesForUser(userId: string) {
     .innerJoin(developers, eq(services.developerId, developers.id))
     .innerJoin(users, eq(developers.email, users.email))
     .where(eq(users.id, userId));
+
+  asDeveloper.forEach((service) => {
+    /* @ts-expect-error new property*/
+    service.reviews = allReviews
+      .filter((reviewItem) => reviewItem.review.serviceId === service.id)
+      .map((reviewItem) => reviewItem.review);
+  });
 
   return { asClient, asDeveloper };
 }
